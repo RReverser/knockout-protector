89f56b1cf75337153bef5be421d6b8995d5618c0
Ingvar Stepanyan <me@rreverser.com>
2013-09-13 17:00:13 +0300

    Added accept/revert code.

--- ko.protector.js
+++ ko.protector.js
@@ -33,13 +33,63 @@
 		// <script> tag: use the global `ko` object, attaching a `mapping` property
 		factory(ko, ko.protector = {});
 	}
-}(function (ko, exports) {
+}(function (ko, protector) {
 
-	if (typeof (ko) === undefined) {
+	if (ko === undefined) {
 		throw 'Knockout is required, please ensure it is loaded before loading this protector plug-in';
 	}
 
 	// create our namespace object
-	var protector = exports;
 	ko.protector = protector;
+
+	ko.extenders.protector = function (target) {
+		var temp = target.temp = ko.observable();
+		var wasChanged = ko.observable(true);
+
+		// pushing local changes to owner observable
+		temp.accept = function (accept) {
+			if (!wasChanged()) return;
+			accept !== false ? target(temp()) : temp(target());
+			wasChanged(false);
+		};
+
+		// reverting local changes to owner's state
+		temp.revert = function () {
+			temp.accept(false);
+		};
+
+		// committing all the third-party changes to temporary observable when it was not changed by user
+		ko.computed(function () {
+			if (wasChanged()) return;
+			temp(target());
+		});
+
+		return target;
+	};
+
+	function traverseProtected(viewModel, method) {
+		for (var name in viewModel) {
+			var value = viewModel[name];
+			if (!value || value.nodeType) continue;
+
+			if (ko.isObservable(value)) {
+				if (ko.isObservable(value.temp)) method(value);
+			} else {
+				traverseProtected(value, method);
+			}
+		}
+	}
+
+	protector.accept = function (viewModel) {
+		traverseProtected(viewModel, function (accessor) {
+			accessor.accept();
+		});
+	};
+
+	protector.revert = function (viewModel) {
+		traverseProtected(viewModel, function (accessor) {
+			accessor.revert();
+		});
+	};
+
 });
